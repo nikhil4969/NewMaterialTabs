ed36afbbac61d12168e88d16f4d7564c2738701c
nikhil4969 <nikhilmahesh4969@gmailcom>
2017-02-10 14:33:37 +0530

    Commit at 14:20

--- app/src/main/java/info/androidhive/materialtabs/activity/MainActivity.java
+++ app/src/main/java/info/androidhive/materialtabs/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -33,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnIconTabs.setOnClickListener(this);
         btnCustomIconTextTabs.setOnClickListener(this);
 
-        // this is the new file
+        Log.i("Hello" , "World");
 
     }
 
