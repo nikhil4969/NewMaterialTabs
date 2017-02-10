9eeb7362732b50c3b0a2ad92e5025bea0787f08e
nikhil4969 <nikhilmahesh4969@gmailcom>
2017-02-10 14:20:31 +0530

    Commit at 14:20

--- app/src/main/java/info/androidhive/materialtabs/activity/MainActivity.java
+++ app/src/main/java/info/androidhive/materialtabs/activity/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnIconTabs.setOnClickListener(this);
         btnCustomIconTextTabs.setOnClickListener(this);
 
+        // this is the new file
+
     }
 
     @Override
